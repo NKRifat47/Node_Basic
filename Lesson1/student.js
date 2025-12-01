ae54d6952ed48401ab7a6462a7b2f42c433d0dec
NKRifat47 <nk.rifat47@gmail.com>
2025-12-01 14:15:52 +0600

    Lesson 1 complete

---
+++ Lesson1/student.js
@@ -0,0 +1,19 @@
+exports.getName = () => {
+    return "MD. Nowazesh Kobir Rift.";
+}
+
+const getAge = () => {
+    return "25";
+}
+
+const cgpa = 3.55;
+
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.result = cgpa;
+
+// module.exports = {
+//     getName,
+//     getAge,
+//     cgpa
+// }
