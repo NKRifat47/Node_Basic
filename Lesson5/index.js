ac20efa63e034cfbe1013aa995f6634e7c6c62a3
NKRifat47 <nk.rifat47@gmail.com>
2025-12-02 01:36:09 +0600

    Updated lesson 5

--- Lesson5/index.js
+++ Lesson5/index.js
@@ -3,30 +3,35 @@ const fs = require("fs")
 const port= 3030
 const hostname = "127.0.0.1"
 
+const handelReadFile = (statusCode, filelocation, req, res) => {
+    fs.readFile(filelocation, "utf-8", (err, data) => {
+       if (err) {
+        console.log(err);
+       }
+       else{
+           res.writeHead(statusCode, { "Content-Type": "text/html" });
+           res.write(data);
+           res.end();
+       }
+    })
+}
+
 const server = http.createServer((req, res)=>{
     
-    const handelReadFile = (statusCode, filelocation) => {
-        fs.readFile(filelocation, (err, data) => {
-            res.writeHead(statusCode , { "Content-Type": "text/html" });
-            res.write(data);
-            res.end();
-        })
-    }
-
     if(req.url === "/"){
-        handelReadFile(200, "./views/index.html")
+        handelReadFile(200, "./views/index.html", req, res)
     }
 
     else if(req.url === "/about"){
-        handelReadFile(200, "./views/about.html")
+        handelReadFile(200, "./views/about.html", req, res)
     }
 
     else if (req.url === "/contact"){
-        handelReadFile(200, "./views/contact.html")
+        handelReadFile(200, "./views/contact.html", req, res)
     }
 
     else {
-        handelReadFile(404, "./views/error.html")
+        handelReadFile(404, "./views/error.html", req, res)
     }
 })
 
