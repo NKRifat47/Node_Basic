cbe8359de2cc74a62ed1423c0b6df473376c1e52
NKRifat47 <nk.rifat47@gmail.com>
2025-12-01 16:06:21 +0600

    lesson 4 updated

--- lesson4/server.js
+++ lesson4/server.js
@@ -3,7 +3,9 @@ const port = 3030
 const hostname = '127.0.0.1'
 
 const myServer = http.createServer((req, res) => {
-    res.end("Hi. This is my first server.")
+    res.writeHead(202, {'content-type':'text/plain'})
+    res.write("hello.")
+    res.end()
 })
 
 myServer.listen(port, hostname, () => {
