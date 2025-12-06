95d6143a7c3ae834cae71a0657c66ec4903a629e
NKRifat47 <nk.rifat47@gmail.com>
2025-12-06 16:44:41 +0600

    mogodb is take place

--- File_upload/index.js
+++ File_upload/index.js
@@ -1,8 +1,38 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const multer  = require('multer')
+
 
 const app = express();
 const PORT = 5000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+const connectDB = async () =>{
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/test_image");
+        console.log("DB is connected");
+    } catch (error) {
+        console.log("DB is not connected");
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required: [true, "User name is required"]
+    },
+    image: {
+        type : String,
+        required: [true, "Image is required"]
+    }
+})
+
+const User = mongoose.model("user", userSchema);
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -23,10 +53,20 @@ app.get("/register", (req, res) =>{
     res.status(200).sendFile(__dirname + "/index.html");
 })
 
-app.post("/register", upload.single("image"), (req, res, next) =>{
-    res.status(200).send("file uploaded successfully.")
+app.post("/register", upload.single("image"), async (req, res, next) =>{
+   try {
+    const newuser = new User ({
+        name : req.body.name,
+        image : req.file.filename 
+    });
+    await newuser.save();
+    res.status(201).send(newuser);
+   } catch (error) {
+    res.status(500).send(error.message);
+   }
 })
 
-app.listen(PORT, () =>{
+app.listen(PORT, async () =>{
     console.log(`Server is running at http://localhost:${PORT}`);
+    await connectDB();
 })
